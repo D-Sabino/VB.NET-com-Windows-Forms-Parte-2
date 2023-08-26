5c86754addc36c84452a9d0b0296e8238921f5be
Daniel Sabino <dannisabino17@gmail.com>
2023-08-26 17:37:43 -0300

    Criando nosso projeto

---
+++ OrientacaoObjeto/1 - Primeira Classe/Frm_Principal_01.vb
@@ -0,0 +1,11 @@
+﻿Public Class Frm_Principal_01
+    Public Sub New()
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+
+        ' Adicione qualquer inicialização após a chamada InitializeComponent().
+        Me.Text = "1 - Primeira classe"
+        lbl_Principal.Text = "1 - Primeira classe"
+    End Sub
+End Class
